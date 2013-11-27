1eaca8f1cb0838c805ffe43f2b4526d0331e8446
rafaelfernandesmarcondes@gmail.com <ubuntu@ip-172-31-18-57.sa-east-1.compute.internal>
2013-11-27 00:03:16 +0000

    passport

--- app.js
+++ app.js
@@ -10,6 +10,35 @@ var http = require('http');
 var path = require('path');
 var engine = require('ejs-locals');
 var app = express();
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var mongoose = require('mongoose');
+
+//mongoose
+mongoose.connect('mongodb://localhost/ebopi');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  
+});
+
+
+
+//face
+passport.use(new FacebookStrategy({
+    clientID: '118027284957927',
+    clientSecret: '353a15a3f448b3218d64455463a637ad',
+    callbackURL: "http://www.ebopi.me/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate( function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
+
 
 //twilio
 var accountSid = 'AC90b1a115228aae9f10c2f39653e6f7b8';
@@ -29,6 +58,10 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.session({secret:'ebopi'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
@@ -38,6 +71,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/voice', routes.voice);
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {successRedirect: '/', failureRedirect:'/voice'}));
 
 
 
